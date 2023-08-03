502b3ec54466d72b887129d1b240be5a6fca164c
Shreyas Prasanna <shreyas.prasanna@berkeley.edu>
2023-08-02 23:09:36 -0700

    Initial commit

---
+++ src/main/java/au/com/shreyasprasanna/restfulservice/controller/model/ProductID.java
@@ -0,0 +1,15 @@
+package au.com.shreyasprasanna.restfulservice.controller.model;
+
+public class ProductID {
+
+    private final String id;
+
+    public ProductID(final String id) {
+        this.id = id;
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+}
